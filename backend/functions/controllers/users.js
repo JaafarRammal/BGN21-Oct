4c756d9899b6c2cd88000e8464805a0a767ab6e8
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-10-30 00:42:12 +0100

    Feat: Can request for a list of users. Did this just for a template of what a request should look like

--- backend/functions/controllers/users.js
+++ backend/functions/controllers/users.js
@@ -1,6 +1,6 @@
-const functions = require("firebase-functions");
+// const functions = require("firebase-functions");
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 
 
 const admin = require("firebase-admin");
@@ -8,33 +8,33 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
-const app = express(); //For rest API Calls
+const app = express(); // For rest API Calls
 app.use(cors({origin: true}));
 
-//GET REQUESTS
-app.get('/', async (request, response) => {
-  const snapchot = await db.collection("users").get();
-  let users = [];
-  snapshot.forEach(doc =>{
-    let id = doc.id;
-    let data = doc.data;
+// GET REQUESTS
+app.get("/", async (request, response) => {
+  const snapshot = await db.collection("users").get();
+  const users = [];
+  snapshot.forEach((doc) =>{
+    const id = doc.id;
+    const data = doc.data;
     users.push({id, ...data});
   });
 
-  res.status(200).send(JSON.stringify(users));
+  response.status(200).send(JSON.stringify(users));
 });
 
-//POST REQUESTS
-app.post('/userprofile/', (request, response) => {
-
-});
-
-//DELETE REQUESTS
-app.delete('/', (request, response) => {
+// POST REQUESTS
+app.post("/userprofile/", (request, response) => {
 
 });
 
-//PUT REQUESTS
-app.put('/', (request, response) => {
+// DELETE REQUESTS
+app.delete("/", (request, response) => {
+
+});
+
+// PUT REQUESTS
+app.put("/", (request, response) => {
 
 });
--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-  ...require('.controllers/user')
-}
+  ...require("./controllers/users"),
+};
