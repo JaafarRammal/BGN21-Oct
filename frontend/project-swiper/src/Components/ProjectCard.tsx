777a96f386e9a6ec85cdb186427e4c26f8cf8a5a
saad-imran98 <saad2379@gmail.com>
2021-10-30 16:01:03 +0100

    Project card dialog stuff

--- frontend/project-swiper/src/Components/ProjectCard.tsx
+++ frontend/project-swiper/src/Components/ProjectCard.tsx
@@ -8,13 +8,49 @@ import IconButton, {IconButtonProps} from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import {red} from '@mui/material/colors';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import {Chip, Divider, Stack} from "@mui/material";
+import {Button, Chip, Divider, Stack} from "@mui/material";
 import logo from "../Assets/logo.png";
+import Box from '@mui/material/Box';
+import Dialog, { DialogProps } from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Switch from '@mui/material/Switch';
 
 
 export default function ProjectCard(props: any) {
 
     const {title, description, date, languages, tags} = props;
+    const [open, setOpen] = React.useState(false);
+    const [fullWidth, setFullWidth] = React.useState(true);
+    const [maxWidth, setMaxWidth] = React.useState<DialogProps['maxWidth']>('sm');
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        console.log('close clicked');
+        setOpen(false);
+        console.log(open);
+    };
+
+    const handleMaxWidthChange = (event: SelectChangeEvent<typeof maxWidth>) => {
+        setMaxWidth(
+            // @ts-expect-error autofill of arbitrary value is not handled.
+            event.target.value,
+        );
+    };
+
+    const handleFullWidthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFullWidth(event.target.checked);
+    }
 
     const Tags = tags.map((tag: string) => {
         return <Chip size="small" label={tag} color="primary"/>
@@ -25,7 +61,7 @@ export default function ProjectCard(props: any) {
     });
 
     return (
-        <Card sx={{maxWidth: 500}}>
+        <Card sx={{maxWidth: 500}} onClick={handleClickOpen}>
             <CardHeader
                 avatar={
                     <Avatar sx={{bgcolor: red[500]}} aria-label="recipe">
@@ -53,6 +89,65 @@ export default function ProjectCard(props: any) {
                     {Tags}
                 </Stack>
             </CardContent>
+
+                <Button variant="outlined" onClick={handleClickOpen}>
+                    Open max-width dialog
+                </Button>
+                <Dialog
+                    fullWidth={fullWidth}
+                    maxWidth={maxWidth}
+                    open={open}
+                    onClose={() => handleClose()}
+                >
+                    <h1>{open}</h1>
+                    <DialogTitle>Optional sizes</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            You can set my maximum width and whether to adapt or not.
+                        </DialogContentText>
+                        <Box
+                            noValidate
+                            component="form"
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                m: 'auto',
+                                width: 'fit-content',
+                            }}
+                        >
+                            <FormControl sx={{ mt: 2, minWidth: 120 }}>
+                                <InputLabel htmlFor="max-width">maxWidth</InputLabel>
+                                <Select
+                                    autoFocus
+                                    value={maxWidth}
+                                    onChange={handleMaxWidthChange}
+                                    label="maxWidth"
+                                    inputProps={{
+                                        name: 'max-width',
+                                        id: 'max-width',
+                                    }}
+                                >
+                                    <MenuItem value={false as any}>false</MenuItem>
+                                    <MenuItem value="xs">xs</MenuItem>
+                                    <MenuItem value="sm">sm</MenuItem>
+                                    <MenuItem value="md">md</MenuItem>
+                                    <MenuItem value="lg">lg</MenuItem>
+                                    <MenuItem value="xl">xl</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <FormControlLabel
+                                sx={{ mt: 1 }}
+                                control={
+                                    <Switch checked={fullWidth} onChange={handleFullWidthChange} />
+                                }
+                                label="Full width"
+                            />
+                        </Box>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Close</Button>
+                    </DialogActions>
+                </Dialog>
         </Card>
     );
 }
