9e31893a537b63883bc67c6453d280aabbb58bf4
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-10-30 10:36:35 +0100

    Feat: Implemented a basic mathcing algorithm

--- backend/functions/controllers/users.js
+++ backend/functions/controllers/users.js
@@ -1,3 +1,7 @@
+import {
+  match
+} from "./utils/match.js"
+
 // const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
@@ -9,19 +13,32 @@ admin.initializeApp();
 const db = admin.firestore();
 
 const app = express(); // For rest API Calls
-app.use(cors({origin: true}));
+app.use(cors({
+  origin: true
+}));
 
 // GET REQUESTS
 app.get("/", async (request, response) => {
   const snapshot = await db.collection("users").get();
   const users = [];
-  snapshot.forEach((doc) =>{
+  snapshot.forEach((doc) => {
     const id = doc.id;
     const data = doc.data;
-    users.push({id, ...data});
+    users.push({
+      id,
+      ...data
+    });
   });
 
   response.status(200).send(JSON.stringify(users));
+});
+
+app.get("/:id", async (request, response) => {
+  const projects = await db.collection("projects").get();
+  const user = db.collection("users").doc(request.params.id).get()
+  matchedProjects = match(user, projects);
+
+  response.status(200).send(JSON.stringify(matchedProjects));
 });
 
 // POST REQUESTS
