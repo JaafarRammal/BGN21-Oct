14b3759279da20b78fd2cd9248e614b348963f6e
saad-imran98 <saad2379@gmail.com>
2021-10-30 14:11:32 +0100

    Project card

--- frontend/project-swiper/src/Components/ProjectCard.tsx
+++ frontend/project-swiper/src/Components/ProjectCard.tsx
@@ -1,17 +1,58 @@
-export default function ProjectCard(props: any) {
-  const { title, description } = props;
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
+import IconButton, {IconButtonProps} from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import {red} from '@mui/material/colors';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import {Chip, Divider, Stack} from "@mui/material";
+import logo from "../Assets/logo.png";
 
-  return (
-    <div className="card text-center" style={{ maxWidth: "800px", margin: "20px 20px 10px 20px" }}>
-      <div className="card-header">{title}</div>
-      <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
-        <p className="card-text">{description}</p>
-        <a href="/" className="btn btn-primary">
-          Go somewhere
-        </a>
-      </div>
-      <div className="card-footer text-muted">2 days ago</div>
-    </div>
-  );
+
+export default function ProjectCard(props: any) {
+
+    const {title, description, date, languages, tags} = props;
+
+    const Tags = tags.map((tag: string) => {
+        return <Chip size="small" label={tag} color="primary"/>
+    });
+
+    const Languages = languages.map((language: string) => {
+        return <Chip size="small" label={language} variant="outlined" color="secondary"/>
+    });
+
+    return (
+        <Card sx={{maxWidth: 500}}>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{bgcolor: red[500]}} aria-label="recipe">
+                        R
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon/>
+                    </IconButton>
+                }
+                title={title}
+                subheader={date}
+            />
+            <CardMedia
+                component="img"
+                height="194"
+                image={logo}
+                style={{maxWidth:"500px"}}
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">{description}</Typography>
+                <Stack direction="row" spacing={1}>
+                    {Languages}
+                    {Tags}
+                </Stack>
+            </CardContent>
+        </Card>
+    );
 }
--- frontend/project-swiper/src/Components/SwipeableCard.tsx
+++ frontend/project-swiper/src/Components/SwipeableCard.tsx
@@ -5,7 +5,14 @@ export default function SwipeableCard() {
   return (
     <Swiper slidesPerView={1} onSlideChange={() => console.log("slide change")} onSwiper={(swiper) => console.log(swiper)}>
       <SwiperSlide>
-        <ProjectCard title="Custom title" description="A little description" />
+        <ProjectCard
+            title="Custom title"
+            description="A little description about the project and how it will help
+            you understand the fundamentals of x technology and y engineering principles."
+            date = {new Date().toISOString().split('T')[0]}
+            languages = {['Python','Java','Rust']}
+            tags = {['Fullstack','WebApp']}
+        />
       </SwiperSlide>
     </Swiper>
   );
